2cb59b98880fe95a5fbc22b992303e55a0045113
Ben Petrosky and Starlynne Start <ben+starlynne@>
2017-04-26 10:28:02 -0700

    Build out dealership functionality for objects within objects

--- app.rb
+++ app.rb
@@ -2,26 +2,52 @@ require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/vehicle')
+require('./lib/dealership')
 
 get('/') do
   erb(:index)
 end
-
-get('/vehicles') do
-  @vehicles = Vehicle.all()
-  erb(:vehicles)
+get('/dealerships') do
+  @dealerships = Dealership.all()
+  erb(:dealerships)
 end
-
-get('/vehicles/new') do
-  erb(:vehicles_form)
+get('/dealerships/new') do
+   erb(:dealerships_form)
 end
+post('/dealerships') do
+  name = params.fetch('name')
+  Dealership.new(name).save()
+  @dealerships = Dealership.all()
+  erb(:success)
+end
+get('/vehicles/:id') do
+  @vehicle = Vehicle.find(params.fetch('id').to_i())
+  erb(:one_vehicle)
+end
+get('/dealerships/:id') do
+  @dealership = Dealership.find(params.fetch('id').to_i())
+  erb(:one_dealership)
+end
+get('/dealerships/:id/vehicles/new') do
+    @dealership = Dealership.find(params.fetch('id').to_i())
+    erb(:dealership_vehicles_form)
+end
+# get('/vehicles') do
+#   @vehicles = Vehicle.all()
+#   erb(:vehicles)
+# end
+# get('/vehicles/new') do
+#   erb(:vehicles_form)
+# end
 
 post('/vehicles') do
   @make = params.fetch("make")
   @model = params.fetch("model")
   @year = params.fetch("year")
-  vehicle = Vehicle.new(@make, @model, @year)
-  vehicle.save()
+  @vehicle = Vehicle.new(@make, @model, @year)
+  @vehicle.save()
+  @dealership = Dealership.find(params.fetch('dealership_id').to_i())
+  @dealership.add_vehicle(@vehicle)
   erb(:success)
 end
 
